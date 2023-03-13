6ed90f30aa429e5bb223c024f07ce273803eb6fa
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-13 14:58:19 -0500

    added serializers to sample list

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -2,36 +2,38 @@ import { Link, useNavigate } from "react-router-dom"
 import "./NavBar.css"
 
 export const NavBar = () => {
-    const navigate = useNavigate()
-    return (
-        <ul className="navbar">
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            <li className="navbar__item">
-                Navigation link
-            </li>
-            {
-                (localStorage.getItem("lu_token") !== null) ?
-                    <li className="nav-item">
-                        <button className="nav-link fakeLink"
-                            onClick={() => {
-                                localStorage.removeItem("lu_token")
-                                navigate('/login')
-                            }}
-                        >Logout</button>
-                    </li> :
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                    </>
-            }        </ul>
-    )
+  const navigate = useNavigate()
+  return (
+    <ul className="navbar">
+      <div className="logo">SamplStak</div>
+      <li className="navbar__item">Browse</li>
+      <li className="navbar__item">My Sounds</li>
+      {localStorage.getItem("lu_token") !== null ? (
+        <li className="nav-item">
+          <button
+            className="nav-link fakeLink"
+            onClick={() => {
+              localStorage.removeItem("lu_token")
+              navigate("/login")
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      ) : (
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </li>
+        </>
+      )}{" "}
+    </ul>
+  )
 }
