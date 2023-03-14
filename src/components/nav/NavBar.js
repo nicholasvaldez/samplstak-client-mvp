492fc6c93fae2254491ee95a2c54d60116a72b6a
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-14 10:22:55 -0500

    instrument dropdown

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,36 +6,34 @@ export const NavBar = () => {
   return (
     <ul className="navbar">
       <div className="logo">SamplStak</div>
-      <div className="navbar__items">
-        <li className="navbar__item">Browse</li>
-        <li className="navbar__item">My Sounds</li>
-        {localStorage.getItem("lu_token") !== null ? (
+      <li className="navbar__item">Browse</li>
+      <li className="navbar__item">My Sounds</li>
+      {localStorage.getItem("lu_token") !== null ? (
+        <li className="nav-item">
+          <button
+            className="nav-link fakeLink"
+            onClick={() => {
+              localStorage.removeItem("lu_token")
+              navigate("/login")
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      ) : (
+        <>
           <li className="nav-item">
-            <button
-              className="nav-link fakeLink"
-              onClick={() => {
-                localStorage.removeItem("lu_token")
-                navigate("/login")
-              }}
-            >
-              Logout
-            </button>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
           </li>
-        ) : (
-          <>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
-          </>
-        )}{" "}
-      </div>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </li>
+        </>
+      )}{" "}
     </ul>
   )
 }
