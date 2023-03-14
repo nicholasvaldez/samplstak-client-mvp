7ce042f2d4049766d42304a845bdc26d9520d225
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-14 09:49:52 -0500

    add sample component within sample list

--- src/components/sample/SampleList.js
+++ src/components/sample/SampleList.js
@@ -2,40 +2,32 @@ import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import { getSamples } from "../../managers/SampleManager"
+import { Samples } from "./Samples"
 import "./samples.css"
 
 export const SampleList = (props) => {
   const [samples, setSamples] = useState([])
+  const [instruments, setInstruments] = useState([])
   const navigate = useNavigate()
+
   useEffect(() => {
     getSamples().then((data) => setSamples(data))
   }, [])
 
   return (
     <>
+      i
       <article className="samples">
-        {samples.map((sample) => {
-          return (
-            <section key={`sample--${sample.id}`} className="sample">
-              <div className="sample__play">
-                <h2 className="sample__play-button">
-                  <a href={sample.file_url}>&gt;</a>
-                </h2>
-              </div>
-              <div className="sample__url">
-                {sample.file_name} by {sample.producer}
-              </div>
-
-              <div className="sample__instrument">
-                {sample.instrument.label}
-              </div>
-              <div className="sample__genre">
-                {sample.genre.map((genre) => genre.label).join(", ")}
-              </div>
-              <button className="button">+</button>
-            </section>
-          )
-        })}
+        {samples.map((s) => (
+          <Samples
+            key={s.id}
+            fileUrl={s.file_url}
+            fileName={s.file_name}
+            producer={s.producer}
+            instrument={s.instrument.label}
+            genre={s.genre.map((g) => g.label).join(", ")}
+          />
+        ))}
       </article>
     </>
   )
